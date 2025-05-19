5f43288ad7d405ad8284c288fe72169a6e30fc41
Juan Carlos <jccandelabordera@gmail.com>
2025-05-20 00:19:57 +0200

    Creation popup done

---
+++ Beyond.Todo.WebApp/Beyond.Todo.WebApp.Client/Models/CreateTodoDto.cs
@@ -0,0 +1,8 @@
+namespace Beyond.Todo.WebApp.Client.Models;
+
+public class CreateTodoDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+}
